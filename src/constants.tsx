16ea488acf71474410a4fca97e39057b17975c53
EzeMmuo <olekakamsy@gmail.com>
2021-06-10 10:23:10 +0100

    removed null before email, handled send.invalid_receipt

--- src/constants.tsx
+++ src/constants.tsx
@@ -2,5 +2,12 @@ const INSUFFICIENT_FUNDS = 'send.insufficient_funds';
 const USER_INSUFFICIENT_FUNDS = 'send.user_insufficient_funds';
 const SUCCESS = 'send.success';
 const ERROR = 'send.error';
+const INVALID_RECEIPT = 'send.invalid_receipt';
 
-export { INSUFFICIENT_FUNDS, USER_INSUFFICIENT_FUNDS, SUCCESS, ERROR };
+export {
+  INSUFFICIENT_FUNDS,
+  USER_INSUFFICIENT_FUNDS,
+  SUCCESS,
+  ERROR,
+  INVALID_RECEIPT,
+};
