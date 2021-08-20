ea9398a680ae73ab8dac2adf3cf7955c53f32f05
EzeMmuo <olekakamsy@gmail.com>
2021-08-20 13:41:39 +0100

    V2 SDK: - added direct charge SDK - switched to webview - added support for meta data (optional) - handling errors/crashes with webvew - removed old code - updated README - added License - removed husky pre-commit checker

---
+++ src/components/FeaturesWrapper.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View, Modal, StyleSheet, Platform } from 'react-native';
+import type { FeaturesWrapperProps } from '../@types';
+
+const FeaturesWrapper = ({
+  visible,
+  onRequestClose,
+  children,
+}: FeaturesWrapperProps) => {
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      {...{ visible, onRequestClose }}
+    >
+      <View style={styles.container}>
+        <View style={styles.mainView}>{children}</View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.6)',
+    paddingTop: Platform.OS === 'ios' ? 48 : 36,
+    position: 'relative',
+  },
+  mainView: {
+    position: 'relative',
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    backgroundColor: '#fff',
+    overflow: 'hidden',
+    height: '100%',
+  },
+});
+
+export default FeaturesWrapper;
--- src/constants.tsx
+++ src/constants.tsx
@@ -1,11 +1,14 @@
-const INSUFFICIENT_FUNDS = 'send.insufficient_funds';
-const USER_INSUFFICIENT_FUNDS = 'send.user_insufficient_funds';
-const SUCCESS = 'send.success';
-const ERROR = 'send.server_error';
-
+const SEND_SUCCESS = 'send.success';
+const SEND_ERROR = 'send.server_error';
+const SEND_CLOSE = 'send.close';
+const DIRECT_DEBIT_SUCCESS = 'direct_debit.success';
+const DIRECT_DEBIT_ERROR = 'direct_debit.server_error';
+const DIRECT_DEBIT_CLOSE = 'direct_debit.close';
 export {
-  INSUFFICIENT_FUNDS,
-  USER_INSUFFICIENT_FUNDS,
-  SUCCESS,
-  ERROR,
+  SEND_SUCCESS,
+  SEND_ERROR,
+  SEND_CLOSE,
+  DIRECT_DEBIT_SUCCESS,
+  DIRECT_DEBIT_ERROR,
+  DIRECT_DEBIT_CLOSE,
 };
