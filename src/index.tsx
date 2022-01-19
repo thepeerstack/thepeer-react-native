26602b1756081a96357be1ae11c8412af12fe85d
kamsy <olekakamsy@gmail.com>
2022-01-19 20:54:51 +0100

    - removed unused props for Send - added new error types for DC - updated the @types file - added Checkout SDK - updated README

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import ThePeerSend from './features/Send';
 import ThePeerDirectCharge from './features/DirectDebit';
+import ThePeerCheckout from './features/Checkout';
 
-export { ThePeerSend, ThePeerDirectCharge };
+export { ThePeerSend, ThePeerDirectCharge, ThePeerCheckout };
