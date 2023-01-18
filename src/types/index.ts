0c9cc9b631eff0d693646ba74b87a6b72b7581c5
kamsy <olekakamsy@gmail.com>
2023-01-18 11:36:12 +0000

    - updated types - added new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import type { WebViewMessageEvent } from 'react-native-webview';
+
 export interface FeaturesWrapperProps {
   visible: boolean;
   onRequestClose: () => void;
@@ -10,29 +12,58 @@ export interface MetaProps {
 
 export type EventResponse = {
   type: string;
-  data: undefined | Object;
+  event: string;
+  data: Object;
 };
-export interface GeneralProps {
+
+export type Callback = (response: EventResponse) => void;
+
+export interface GeneralProps
+  extends Record<
+    string,
+    string | number | MetaProps | Callback | boolean | undefined
+  > {
   publicKey: string;
   amount: string | number;
   meta?: MetaProps;
   currency?: string;
-  onSuccess: (response: EventResponse) => void;
-  onError: (response: EventResponse) => void;
-  onClose: (response?: EventResponse) => void;
+  sdkType: string;
+  onSuccess: Callback;
+  onError: Callback;
+  onClose: Callback;
+  userReference?: string;
 }
 
-export interface DirectDebitProps {
+export interface DirectDebitProps extends GeneralProps {
   openDirectChargeSDK: boolean;
-  userReference: string;
 }
 
-export interface SendProps {
+export interface SendProps extends GeneralProps {
   openSendSDK: boolean;
-  userReference: string;
 }
 
-export interface CheckoutProps {
+export interface CheckoutProps extends GeneralProps {
   openCheckoutSDK: boolean;
   email: string;
 }
+
+export type WebViewProps = {
+  onClose: (response: EventResponse) => void;
+  source: { uri: string };
+  sdkType: string;
+  onMessage: ((event: WebViewMessageEvent) => void) &
+    ((prop: DispatchedMessage) => void);
+};
+
+export type DispatchedMessage = {
+  nativeEvent: {
+    data: string;
+  };
+};
+
+export type HandleMessage = {
+  data: string;
+  onSuccess: Callback;
+  onError: Callback;
+  onClose: Callback;
+};
